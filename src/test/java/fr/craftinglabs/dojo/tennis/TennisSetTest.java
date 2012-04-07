f0cc96477d1bfc0d48da90dfc6e3725947465497
Antoine <antoine.vernois@gmail.com>
2012-04-07 22:35:58 +0200

    red

--- src/test/java/fr/craftinglabs/dojo/tennis/TennisSetTest.java
+++ src/test/java/fr/craftinglabs/dojo/tennis/TennisSetTest.java
@@ -75,6 +75,13 @@ public class TennisSetTest {
 		assertEquals("Advantage Player B", game.score());
 	}
 	
+	@Test
+	public void testDeuce() {
+		playGame(4, 4);
+		
+		assertEquals("Deuce", game.score());
+	}
+	
 	private void playGame(int playerAScore, int playerBScore) {
 		for(int i = 0; i < playerAScore; i++)
 			game.playerAScores();
